616987e25a3586147613c6b685798971989b5c12
Andrey Sharypov <sharypovandrey@gmail.com>
2023-05-12 16:19:57 +0300

    Update setup.sh

--- decoders/setup.sh
+++ decoders/setup.sh
@@ -16,7 +16,7 @@ fi
 
 if [ ! -d openfst-1.6.3 ]; then
     echo "Download and extract openfst ..."
-    wget http://www.openfst.org/twiki/pub/FST/FstDownload/openfst-1.6.3.tar.gz
+    wget https://src.fedoraproject.org/lookaside/extras/openfst/openfst-1.6.3.tar.gz/sha512/06514fb06aa865d2527dec7095930217d7acb423fb295ed4a4bd7975f652a891940cb701e36ed383218782d922008b2164ce1a1df0590684799ad41707814257/openfst-1.6.3.tar.gz
     tar -xzvf openfst-1.6.3.tar.gz
     echo -e "\n"
 fi
